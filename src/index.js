1a5ecce50a58d695f41acf3e45c5c137dc436d7c
matt94nanari <wind.traveler@hotmail.com>
2022-08-31 22:01:23 +0800

    remove packages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import './i18n'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
